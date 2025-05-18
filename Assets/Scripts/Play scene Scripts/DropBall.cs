c560ce0b43d7f4fd3f0661c67b0d0f352ae42900
PeiYuWang1 <s1132290@o365st.pu.edu.tw>
2025-05-18 23:51:13 +0800

    finish DropBall RigidBody

--- Assets/Scripts/Play scene Scripts/DropBall.cs
+++ Assets/Scripts/Play scene Scripts/DropBall.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class DropBall : MonoBehaviour
 {
+    Rigidbody DropBallRg;
+    public int speed=10 ;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DropBallRg = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DropBallRg.AddForce(Vector3.down * speed, ForceMode.Impulse);
     }
 }
